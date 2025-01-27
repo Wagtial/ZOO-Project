f0ab682e7f6a3a31ba79686d1fac92a4a89061a1
Cesar Benjamin <mathereall@gmail.com>
2025-01-27 01:17:07 -0600

    /etc/nginx/sites-available/default

--- docker/nginx-start.sh
+++ docker/nginx-start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 /usr/sbin/fcgiwrap -s unix:/var/run/fcgiwrap.socket &
+nginx -g 'daemon off;'
+sleep 2
 chown www-data:www-data /var/run/fcgiwrap.socket
 chmod 660 /var/run/fcgiwrap.socket
-nginx -g 'daemon off;'
